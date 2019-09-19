7de69d705110ed6f76c2322065c6580e71300b91
jsan <josanescod@gmail.com>
2019-09-19 15:30:09 +0200

    refactored route and  configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+'use strict'
+module.exports = {
+
+    port: process.env.PORT || 3000,
+    db: process.env.MONGODB || 'mongodb://localhost:27017/shop'
+
+
+}
--- server.js
+++ server.js
@@ -1,33 +1,21 @@
 'use strict'
 
-const express = require('express')
-const bodyParser = require('body-parser')
 /*const sqlite3 = require ('sqlite3')*/
 const mongoose = require('mongoose')
-
-const app = express()
-const port = process.env.PORT || 3000
-const productCtrl = require('./controllers/product')
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-/* peticiones */
-app.get('/api/product',productCtrl.getProducts)
-app.get('/api/product/:productId', productCtrl.getProduct)
-app.post('/api/product', productCtrl.saveProduct)
-app.put('/api/product/:productId', productCtrl.updateProduct)
-app.delete('/api/product/:productId', productCtrl.deleteProduct)
+const app = require ('./app')
+const config = require ('./config')
 
 
-mongoose.connect('mongodb://localhost:27017/shop', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, (err, res) => {
+
+
+mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, (err, res) => {
 
     if (err) {
         return console.log(`Error al conectar a la base de datos: ${err}`)
     }
     console.log('Conexion a labase de dato establecida.')
-    app.listen(port, () => {
-        console.log(`API REST corriendo en  http://localhost:${port}`)
+    app.listen(config.port, () => {
+        console.log(`API REST corriendo en  http://localhost:${config.port}`)
     })
 
 })
