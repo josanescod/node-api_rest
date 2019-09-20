f28e76c91dec0f07aa6979ac649fc02b1e144cdb
jsan <josanescod@gmail.com>
2019-09-20 16:21:50 +0200

    views login product

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
 
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3001,
     db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
     SECRET_TOKEN: 'miclavedetokens'
 
